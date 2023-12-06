3b27c33b863f9095bc21c872c9af955114c35a11
Prajwal <prajwalhassan7@gmail.com>
2023-12-07 00:22:29 +0530

    FIRST COMMIT

---
+++ components/Providers/NextProvider.jsx
@@ -0,0 +1,14 @@
+// app/providers.tsx
+'use client'
+
+import {NextUIProvider} from '@nextui-org/react';
+import {useRouter} from 'next/navigation'
+
+export function Providers({children}) {
+
+  return (
+    <NextUIProvider>
+      {children}
+    </NextUIProvider>
+  )
+}
