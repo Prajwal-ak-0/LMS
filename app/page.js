29690fb7c81d983e7ca3940b9097bcafbbeeb491
Prajwal <prajwalhassan7@gmail.com>
2023-12-06 23:02:06 +0530

    Navbar Updated

--- app/page.js
+++ app/page.js
@@ -1,14 +1,13 @@
 "use client"
 
 import Nav from "@/components/Navbar";
-import { UserButton } from "@clerk/nextjs";
 
 // Main component
 const page = () => {
   return (
     <>
       <div>
-        <UserButton afterSignOutUrl="/" />
+        <Nav/>
       </div>
     </>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,42 +1,24 @@
 import React from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar ,Button} from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar, Button } from "@nextui-org/react";
 import { FaBookReader } from "react-icons/fa";
 import { SearchIcon } from "./Search.jsx";
+import { UserButton } from "@clerk/nextjs";
+import SearchBar from "./SearchBar.jsx";
 
 export default function Nav() {
     return (
-        <Navbar isBordered>
-            <NavbarContent justify="start">
-                <NavbarBrand className="mr-4">
-                    <FaBookReader size={28} />
-                    <p className="hidden sm:block ml-2 font-bold text-inherit">EduHub</p>
-                </NavbarBrand>
-            </NavbarContent>
+        <div className="flex justify-between items-center w-full h-20 dark:shadow-red  dark:shadow-2xl">
+            <div className="flex pl-8">
+                <FaBookReader size={28} />
+                <p className="hidden text-lg pt-1 sm:block pl-2 font-bold text-inherit">EduHub</p>
+            </div>
+            <div className="flex items-center justify-center">
+                <SearchBar />
+            </div>
+            <div className="right-0 pr-6 flex gap-4">
+                <UserButton afterSignOutUrl="/" />
+            </div>
+        </div>
 
-            <NavbarContent as="div" className="items-center" justify="end">
-                <Input
-                    classNames={{
-                        base: "max-w-full sm:max-w-[10rem] h-10",
-                        mainWrapper: "h-full",
-                        input: "text-small",
-                        inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/30",
-                    }}
-                    placeholder="Type to search..."
-                    size="sm"
-                    startContent={<SearchIcon size={18} />}
-                    type="search"
-                />
-
-                <NavbarItem className="hidden lg:flex">
-                    <Link href="#">Login</Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Button as={Link} color="primary" href="#" variant="flat">
-                        Sign Up
-                    </Button>
-                </NavbarItem>
-
-            </NavbarContent>
-        </Navbar>
     );
 }
